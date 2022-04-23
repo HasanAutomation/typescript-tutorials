2f7f2a25cee529c834ae58da07c205dcc85991cd
Hasan Ali <hali@astcorporation.com>
2022-04-23 11:08:36 +0530

    Added first typescript project

---
+++ fetchjson/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const URL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+interface Todo {
+  id: number;
+  title: string,
+  completed: boolean
+}
+
+axios.get(URL).then(response => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+  logTodo(id, title, finished)
+
+})
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with ID : ${id}
+  Has a title of: ${title}
+  Is it finished: ${completed}
+`)
+}
